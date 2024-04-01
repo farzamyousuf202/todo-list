c6da5c90478aedcda6f49538246191438a6f4a6e
farzamyousuf202 <90801520+farzamyousuf202@users.noreply.github.com>
2024-04-02 01:21:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let Add = await inquirer.prompt([
+        { message: "what do you want add in your bucket list", type: "input", name: "todo" },
+        { message: "do you want to add more your bucket wish", type: "confirm", name: "confirm", default: "false" }
+    ]);
+    todos.push(Add.todo);
+    condition = Add.confirm;
+    console.log(todos);
+}
